bbd73e41195bb6b0ac6317a97cb1b61f956d0eda
omerlabs <omergeziciler@gmail.com>
2026-07-28 23:33:19 +0300

    pwa

--- ingilizce-kelime-ogren/sw.js
+++ ingilizce-kelime-ogren/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ada-english-v3';
+const CACHE_NAME = 'ada-english-v2.0';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
