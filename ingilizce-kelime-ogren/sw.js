f73446b70c55e063b63fb5d160eb4c65c02a8f92
omerlabs <omergeziciler@gmail.com>
2026-07-29 00:22:25 +0300

    buton

--- ingilizce-kelime-ogren/sw.js
+++ ingilizce-kelime-ogren/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ada-english-v4.0';
+const CACHE_NAME = 'ada-english-v5.0';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
